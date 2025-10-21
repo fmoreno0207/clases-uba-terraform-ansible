62e523c4ddea541dfd96285f06d06e4e10868f6f
Fernando <fernando.moreno@fcen.uba.ar>
2025-10-21 13:45:41 -0300

    add terraform ansible labs

--- labs-Pracrticos-alumnos/terraform/lxc-proxmox/main.tf
+++ labs-Pracrticos-alumnos/terraform/lxc-proxmox/main.tf
@@ -1,50 +1,3 @@
-# terraform {
-#   required_providers {
-#     proxmox = {
-#       source  = "telmate/proxmox"
-#       version = "3.0.1-rc3"
-#     }
-#   }
-
-#   required_version = ">= 1.5.0"
-# }
-
-# provider "proxmox" {
-#   pm_api_url          = var.pm_api_url
-#   pm_api_token_id     = var.pm_api_token_id
-#   pm_api_token_secret = var.pm_api_token_secret
-#   pm_tls_insecure     = true
-# }
-
-# # 🚀 Crear contenedor Debian desde tu template .tar.zst
-# resource "proxmox_lxc" "debian_container" {
-#   hostname     = var.hostname
-#   target_node  = var.pm_node
-#   ostemplate   = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
-#   password     = "Sendati1234**"
-#   unprivileged = true
-
-#   rootfs {
-#     storage = "local"
-#     size    = "8G"
-#   }
-
-#   memory = 1024
-#   cores  = 2
-#   swap   = 512
-
-#   network {
-#     name   = "eth0"
-#     bridge = "vmbr0"
-#     ip     = "dhcp"
-#   }
-
-#   start = true
-
-#   # ✅ Autenticación por clave SSH
-#   ssh_public_keys = file("~/.ssh/id_rsa.pub")
-# }
-
 terraform {
   required_providers {
     proxmox = {
@@ -63,7 +16,7 @@ provider "proxmox" {
   pm_tls_insecure     = true
 }
 
-# Crear contenedor Debian solo con acceso por usuario y contraseña
+# 🚀 Crear contenedor Debian desde tu template .tar.zst
 resource "proxmox_lxc" "debian_container" {
   hostname     = var.hostname
   target_node  = var.pm_node
@@ -87,17 +40,7 @@ resource "proxmox_lxc" "debian_container" {
   }
 
   start = true
-}
 
-# Ejecuta comandos dentro del contenedor cuando termina la creación
-resource "null_resource" "enable_root_ssh" {
-  provisioner "local-exec" {
-    command = <<EOT
-    pct exec ${proxmox_lxc.debian_container.id} -- bash -c "sed -i 's/^#*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config"
-    pct exec ${proxmox_lxc.debian_container.id} -- bash -c "sed -i 's/^#*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config"
-    pct exec ${proxmox_lxc.debian_container.id} -- systemctl restart ssh || systemctl restart sshd
-    EOT
-  }
-
-  depends_on = [proxmox_lxc.debian_container]
+  # ✅ Autenticación por clave SSH
+  ssh_public_keys = file("~/.ssh/id_rsa.pub")
 }
